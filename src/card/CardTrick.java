b1a37601078c62bcd825de1171bc13a54f375bea
preetkpr <preetkpr@sheridancollege.ca>
2025-05-27 23:30:36 -0400

    card version 1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,6 +4,9 @@
  */
 package card;
 
+import java.util.Random;
+
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -15,12 +18,16 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+         Random rand = new Random();
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+           c.setValue(rand.nextInt(13) + 1); // 1 to 13
+           c.setSuit(Card.SUITS[rand.nextInt(4)]); // 0 to 3
+           magicHand[i] = c;
+           
+           System.out.println(c.getSuit() + " " + c.getValue());
         }
         
         //insert code to ask the user for Card value and suit, create their card
